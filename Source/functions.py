6206472c0152de4607e3b79a42e7ebb64f812471
maihathm <maixuanhathm@gmail.com>
2023-11-05 21:16:01 +0700

    ko merge

--- Source/functions.py
+++ Source/functions.py
@@ -1,116 +1,55 @@
 def sum_list(list):
+  """Tính tổng các phần tử trong list.
 
-    << << << < HEAD
-"""Tính tổng các phần tử trong list.
+  Args:
+    list: List cần tính tổng.
 
-Args:
-  list: List cần tính tổng.
+  Returns:
+    Tổng các phần tử trong list.
+  """
 
-Returns:
-  Tổng các phần tử trong list.
-"""
-
-total = 0
-for sublist in list:
+  total = 0
+  for sublist in list:
     for item in sublist:
-        total += item
-return total
-
+      total += item
+  return total
 
 def create_empty_matrix(rows, cols):
-    """Tạo ma trận trống với kích thước `(rows, cols)`.
+  """Tạo ma trận trống với kích thước `(rows, cols)`.
 
-    Args:
-      rows: Số hàng của ma trận.
-      cols: Số cột của ma trận.
+  Args:
+    rows: Số hàng của ma trận.
+    cols: Số cột của ma trận.
 
-    Returns:
-      Ma trận trống với kích thước `(rows, cols)`.
-    """
+  Returns:
+    Ma trận trống với kích thước `(rows, cols)`.
+  """
 
-    matrix = []
-    for row in range(rows):
-        matrix.append([])
-        for col in range(cols):
-            matrix[row].append(0)
-    return matrix
-
-
-def create_empty_matrix_ones(rows, cols):
-    """Tạo ma trận trống với kích thước `(rows, cols)`.
-
-    Args:
-      rows: Số hàng của ma trận.
-      cols: Số cột của ma trận.
-
-    Returns:
-      Ma trận trống với kích thước `(rows, cols)`.
-    """
-
-    matrix = []
-    for row in range(rows):
-        matrix.append([])
-        for col in range(cols):
-            matrix[row].append(1)
-    return matrix
-
-== == == =
-"""Tính tổng các phần tử trong list.
-
-Args:
-  list: List cần tính tổng.
-
-Returns:
-  Tổng các phần tử trong list.
-"""
-
-total = 0
-for sublist in list:
-    for item in sublist:
-        total += item
-return total
-
-
-def create_empty_matrix(rows, cols):
-    """Tạo ma trận trống với kích thước `(rows, cols)`.
-
-    Args:
-      rows: Số hàng của ma trận.
-      cols: Số cột của ma trận.
-
-    Returns:
-      Ma trận trống với kích thước `(rows, cols)`.
-    """
-
-    matrix = []
-    for row in range(rows):
-        matrix.append([])
-        for col in range(cols):
-            matrix[row].append(0)
-    return matrix
-
+  matrix = []
+  for row in range(rows):
+    matrix.append([])
+    for col in range(cols):
+      matrix[row].append(0)
+  return matrix
 
 def create_empty_matrix_ones(rows, cols):
-    """Tạo ma trận trống với kích thước `(rows, cols)`.
+  """Tạo ma trận trống với kích thước `(rows, cols)`.
 
-    Args:
-      rows: Số hàng của ma trận.
-      cols: Số cột của ma trận.
+  Args:
+    rows: Số hàng của ma trận.
+    cols: Số cột của ma trận.
 
-    Returns:
-      Ma trận trống với kích thước `(rows, cols)`.
-    """
+  Returns:
+    Ma trận trống với kích thước `(rows, cols)`.
+  """
 
-    matrix = []
-    for row in range(rows):
-        matrix.append([])
-        for col in range(cols):
-            matrix[row].append(1)
-    return matrix
-
->> >> >> > origin / main
-
-
+  matrix = []
+  for row in range(rows):
+    matrix.append([])
+    for col in range(cols):
+      matrix[row].append(1)
+  return matrix
+  
 def matrix_multiply(matrix, scalar):
     result = []
     for row in matrix:
@@ -118,7 +57,3 @@ def matrix_multiply(matrix, scalar):
         result.append(new_row)
     return result
 
-<< << << < HEAD
-== == == =
-
->> >> >> > origin / main
